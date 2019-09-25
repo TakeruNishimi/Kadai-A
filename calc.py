ee33a1699bfe27c455806164447d98e237b3cd41
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 11:36:00 +0900

    最大値、最小値、平均値を出力 数列の入力に対応 map関数からリスト内包表記に変更

--- calc.py
+++ calc.py
@@ -1,7 +1,18 @@
 def main():
-    numbers = "1 3 4 5"
-    number_list = map(int, numbers.split())
+    numbers = input("スペースで区切って数字を複数入力してください。　＞　")
+    number_list = [int(i) for i in numbers.split()]
+
+    # 合計
     print(sum_calc(number_list))
+
+    # 最大値
+    print(max_calc(number_list))
+
+    # 最小値
+    print(min_calc(number_list))
+
+    # 平均値
+    print(mean_calc(number_list))
 
 
 def sum_calc(list):
@@ -9,6 +20,29 @@ def sum_calc(list):
     for num in list:
         sum += num
     return sum
+
+
+def max_calc(list):
+    max = list[0]
+    for num in list:
+        if num > max:
+            max = num
+    return max
+
+
+def min_calc(list):
+    min = list[0]
+    for num in list:
+        if num < min:
+            min = num
+    return min
+
+
+def mean_calc(list):
+    sum = sum_calc(list)
+    average = sum / len(list)
+    return average
+
 
 
 if __name__ == '__main__':
