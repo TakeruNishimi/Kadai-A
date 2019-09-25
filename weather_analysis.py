2e8132c74efaa1ad21453ad72240e4fb5ba8ee42
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 11:04:46 +0900

    全国の平均気温を出力 大阪府の駅名を出力 福岡県の平均気温を出力

---
+++ weather_analysis.py
@@ -0,0 +1,44 @@
+def main():
+    # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
+    weather_information = [
+        {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
+        {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
+        {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
+
+        {'prefecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
+        {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
+        {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
+
+        {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
+        {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
+    ]
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+    temp_list = []
+    for spot in weather_information:
+        temp_list.append(spot['temperature'])
+    temp_average = list_average(temp_list)
+    print(f'全国の平均気温は {temp_average}℃です。')
+
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+    station_list = []
+    for spot in weather_information:
+        if (spot['prefecture'] == '大阪府'):
+            station_list.append(spot['station'])
+    print(",".join(station_list))
+
+    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
+    temp_list_fukuoka = []
+    for spot in weather_information:
+        if (spot['prefecture'] == '福岡県'):
+            temp_list_fukuoka.append(spot['temperature'])
+    temp_average = list_average(temp_list_fukuoka)
+    print(f'福岡県の平均気温は {temp_average}℃です。')
+
+
+def list_average(list):
+    return sum(list) / len(list)
+
+
+if __name__ == '__main__':
+    main()
